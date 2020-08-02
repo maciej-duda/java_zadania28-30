42dee6c00f2d4480a30aa3b429822225999633db
Maciej Duda <56764047+maciej-duda@users.noreply.github.com>
2020-08-02 22:03:51 +0200

    spacing fix
    spacing fix

--- src/com/company/SortWord.java
+++ src/com/company/SortWord.java
@@ -1,6 +1,4 @@
 package com.company;
-
-
 import java.util.Arrays;
 
 public class SortWord {
@@ -30,6 +28,5 @@ public class SortWord {
         for(int i = 0;i<middleWordsArray.length;i++) {
             System.out.println(middleWordsArray[i]);
         }
-
     }
 }
